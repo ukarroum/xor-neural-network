132d551b5237e32ad0f5684e98be468206ba6a10
Yassir Karroum <ukarroum17@gmail.com>
2017-01-31 15:37:30 +0100

    Ajout de la fonction d'addition de deux matrices

--- lib/matrix_functs.cpp
+++ lib/matrix_functs.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+using namespace std;
+
+double **addMatrix(double **mat1, double **mat2, int n, int m)
+{
+	double **res = new double*[n];
+	
+	for(int i = 0; i < n; i++)
+	{
+		res[i] = new double[m];
+		for(int j = 0; j < m; j++)
+			res[i][j] = mat1[i][j] + mat2[i][j];
+	}
+
+	return res;
+}
+
+
